872c131caf618f2aa847460875295d88622f3dc0
sambuitr48 <buitragoosoriosamuel@gmail.com>
2024-02-27 16:07:39 -0500

    Second commit

--- src/main/java/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/repository/impl/ProductRepositoryImpl.java
@@ -3,14 +3,27 @@ package repository.impl;
 import application.DatabaseConnection;
 import models.Product;
 import repository.Repository;
-import service.Service;
 import java.sql.*;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepositoryImpl implements Repository <Product>{
     private Connection getConnection() throws SQLException {
         return DatabaseConnection.getInstance();
+    }
+    private Product createProduct(ResultSet resultSet) throws SQLException {
+        Product producto = new Product();
+        producto.setId(resultSet.getString("id"));
+        producto.setName(resultSet.getString("name"));
+        producto.setPrice(resultSet.getDouble("price"));
+        producto.setRegistrationDate(
+                resultSet.getObject("registrationDate") != null ?
+                        resultSet.getDate("registrationDate")
+                                .toInstant()
+                                .atZone(ZoneId.systemDefault())
+                                .toLocalDateTime() : null);
+        return producto;
     }
 
     @Override
@@ -36,7 +49,7 @@ public class ProductRepositoryImpl implements Repository <Product>{
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
-                Product = createProduct(resultSet);
+                product = createProduct(resultSet);
             }
             resultSet.close();
         } catch (SQLException e) {
